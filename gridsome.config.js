df07eac3512f837bfeb76b0792ab347468053ddb
David Couronné <couronne.david@gmail.com>
2020-02-09 17:29:48 +0100

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -26,6 +26,7 @@ module.exports = {
             'active',
             'rich-text',
             'code',
+            'language-text',
             'svg-inline--fa',
             'table',
             'table-striped',
